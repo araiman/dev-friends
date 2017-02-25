51dc1692cf681af226993df142b5137265a44f0d
araiman <misokatukueyo@gmail.com>
2017-02-25 12:21:49 +0900

    add: 一覧画面の表示をとりあえず実現

---
+++ DevFriends/Models/Project.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNet.Identity;
+
+namespace DevFriends.Models
+{
+	[Table("project", Schema = "devfriends")]
+	public class Project
+	{
+		[Key]
+		[Column("id")]
+		public int Id { get; set; }
+
+		[Required]
+		[MaxLength(20)]
+		[Column("owner_id")]
+		public string OwnerId { get; set; }
+
+		[Required]
+		[Column("name")]
+		public string ProjectName { get; set; }
+		
+		[Column("description")]
+		[MaxLength(300)]
+		public string Description { get; set; }
+	}
+}
---
+++ DevFriends/Models/ProjectTagRelation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace DevFriends.Models
+{
+	[Table("tag_project_rltn", Schema = "devfriends")]
+	public class TagProjectRelation
+	{
+		[Key]
+		[Column("tag_id", 
+		Order = 0)]
+		public int TagId { get; set; }
+
+		[Key]
+		[Column("project_id", Order = 1)]
+		public int ProjectId { get; set; }
+	}
+}
