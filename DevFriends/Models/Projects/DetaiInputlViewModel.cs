837759eb4f6987aa145760f3426037a03aee0dd4
araiman <misokatukueyo@gmail.com>
2017-04-01 12:34:19 +0900

    add: とりあえず募集案件の詳細ページを表示できるようにした

---
+++ DevFriends/Models/Projects/DetaiInputlViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace DevFriends.Models.Projects
+{
+	public class DetailInputViewModel
+	{
+		public Project Project { get; set; }
+
+		public User Owner { get; set; }
+
+		public IEnumerable<Tag> Tags { get; set; }
+
+		public IEnumerable<Project> RelatedProjects { get; set; }
+
+		public string HashedOwnerEmailForProfileImage { get; set; }
+	}
+}
