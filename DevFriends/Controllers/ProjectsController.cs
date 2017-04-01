6e30be830a5ccc07a6ee3c6b7b04c821b1f06fba
araiman <misokatukueyo@gmail.com>
2017-04-01 12:40:06 +0900

    clean: コードをリファクタリング

--- DevFriends/Controllers/ProjectsController.cs
+++ DevFriends/Controllers/ProjectsController.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Data.Entity.Core.Objects.DataClasses;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Web.Configuration;
 using System.Web.Mvc;
 using DevFriends.Models;
 using DevFriends.Models.Projects;
-using WebGrease.Css.Extensions;
 
 namespace DevFriends.Controllers
 {
@@ -17,8 +13,7 @@ namespace DevFriends.Controllers
 	{
 		public ActionResult Index()
 		{
-			var indexViewmodel = new IndexViewModel();
-
+			var projectsWithContexts = new List<ProjectWithContext>();
 			using (var projectsContext = new ProjectsContext())
 			{
 				var projectsWithOwnerName = projectsContext.Projects.Join(
@@ -27,26 +22,31 @@ namespace DevFriends.Controllers
 					u => u.UserId,
 					(p, u) => new
 					{
-						p.Id,
-						p.ProjectName,
-						p.Description,
-						OwnerName = u.Name,
-					});
+						Project = p,
+						OwnerName = u.Name
+					}).ToList();
 
-				var tagsWithProjectId = projectsContext.Tags.Join(
-					projectsContext.TagProjectRelations,
-					t => t.Id,
-					r => r.TagId,
-					(t, r) => new
+				foreach (var projectWithOwnerName in projectsWithOwnerName)
+				{
+					var tagNames = projectWithOwnerName.Project.TagProjectRelations.Join(
+						projectsContext.Tags,
+						r => r.TagId,
+						t => t.Id,
+						(r, t) => t.Name
+						).ToList();
+
+					projectsWithContexts.Add(new ProjectWithContext
 					{
-						r.ProjectId,
-						t.Name
+						Project = projectWithOwnerName.Project,
+						OwnerName = projectWithOwnerName.OwnerName,
+						TagNames = tagNames
 					});
+				}
+			}
 
-			return View(new IndexViewModel { ProjectsWithContext = projectsWithContexts });
+			return View(new IndexViewModel {ProjectsWithContext = projectsWithContexts});
 		}
 
-		// todo そもそも直接呼び出すことを制御するか何かしらのバリデーションが必要
 		public ActionResult Detail(int projectId, string ownerId, int tagIdForRelatedProjects)
 		{
 			var detailInputViewModel = new DetailInputViewModel();
@@ -55,16 +55,17 @@ namespace DevFriends.Controllers
 			{
 				// Project
 				detailInputViewModel.Project = projectsContext.Projects.Single(p => p.Id == projectId);
-				
+
 				detailInputViewModel.Owner = projectsContext.Users.Single(u => u.UserId == ownerId);
 
-				detailInputViewModel.Tags = projectsContext.Tags.Where(t => t.TagProjectRelations.Any(r => r.ProjectId == projectId)).ToList();
+				detailInputViewModel.Tags =
+					projectsContext.Tags.Where(t => t.TagProjectRelations.Any(r => r.ProjectId == projectId)).ToList();
 
 				var relatedProjects = new List<Project>();
 				foreach (var project in projectsContext.Projects.ToList())
 				{
 					if (project.TagProjectRelations.Any(r => r.TagId == tagIdForRelatedProjects))
-						relatedProjects.Add(project);	
+						relatedProjects.Add(project);
 				}
 
 				var relatedProjectsCountToTook = relatedProjects.Count() >= 3 ? 3 : relatedProjects.Count();
@@ -73,7 +74,8 @@ namespace DevFriends.Controllers
 			}
 
 			var md5 = new MD5CryptoServiceProvider();
-			var hasedEmail = BitConverter.ToString(md5.ComputeHash(Encoding.Default.GetBytes(detailInputViewModel.Owner.Email.Trim().ToLower())));
+			var hasedEmail =
+				BitConverter.ToString(md5.ComputeHash(Encoding.Default.GetBytes(detailInputViewModel.Owner.Email.Trim().ToLower())));
 			var fromattedEmailForGravatar = hasedEmail.Replace("-", "").ToLower();
 			detailInputViewModel.HashedOwnerEmailForProfileImage = fromattedEmailForGravatar;
 
